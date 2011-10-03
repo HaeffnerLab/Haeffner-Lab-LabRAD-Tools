01f7e143014ecd08bdd77c0d531e660ecf32af52
Soenke Moeller <soenkeamoeller@gmail.com>
2011-10-03 15:49:29 -0700

    writeline in serialdeviceserver

--- serialdevices/serialdeviceserver.py
+++ serialdevices/serialdeviceserver.py
@@ -104,6 +104,7 @@ class SerialDeviceServer( LabradServer ):
             if timeout is not None: ser.timeout( timeout )
             if baudrate is not None: ser.baudrate( baudrate )
             self.write = lambda s: ser.write( s )
+            self.writeline = lambda s: ser.write_line(s)
             self.read = lambda x = 0: ser.read( x )
             self.readline = lambda: ser.read_line()
             self.close = lambda: ser.close()
