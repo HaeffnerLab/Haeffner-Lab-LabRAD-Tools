b064b38320e6ffa887648adc0d8d82893f38f4d3
Michael Ramm <micramm@gmail.com>
2011-12-09 11:49:49 -0800

    cleaned by PB server, TRFPGA now returns timetags format

--- okfpgaservers/time_resolved_ok.py
+++ okfpgaservers/time_resolved_ok.py
@@ -1,12 +1,11 @@
 #Created on Aug 08, 2011
 #@author: Michael Ramm, Haeffner Lab
 #Thanks for code ideas from Quanta Lab, MIT
-
-"""
+'''
 ### BEGIN NODE INFO
 [info]
 name = TimeResolvedFPGA
-version = 1.0
+version = 2.0
 description = 
 instancename = TimeResolvedFPGA
 
@@ -18,7 +17,7 @@ timeout = 20
 message = 987654321
 timeout = 20
 ### END NODE INFO
-"""
+'''
 
 import ok
 from labrad.server import LabradServer, setting
@@ -90,7 +89,8 @@ class TimeResolvedFPGA(LabradServer):
         self.singleReadingDeferred = Deferred()
         yield deferToThread(self._resetBoard)
         reactor.callLater(0, self.doSingleReading, buflength)
-        yield deferToThread(time.sleep, 0.020) #delay to make sure the previous command got to the  ReadFromBlockPipeOut line
+        #delay to make sure the previous command got to the  ReadFromBlockPipeOut line and FPGA is ready to be triggered
+        yield deferToThread(time.sleep, 0.020) 
     
     @inlineCallbacks
     def doSingleReading(self, buflength):
@@ -100,6 +100,7 @@ class TimeResolvedFPGA(LabradServer):
         self.xem.ActivateTriggerIn(0x40,0) #reset the board and FIFO
         
     def _singleReading(self, buflength):
+        #xem API does not allow for timeouts, so the only way to get out of ReadFromBlockPipeOut is to have the function complete
         buf = '\x00'*buflength
         self.xem.ReadFromBlockPipeOut(0xa0,1024,buf)
         self.inRequest = False
@@ -122,30 +123,19 @@ class TimeResolvedFPGA(LabradServer):
         times = powers * 8 * timeResolution
         return times
         
-    @setting(1, 'Get Result of Measurement', returns = '((wvv)?)')
+    @setting(1, 'Get Result of Measurement', returns = '*v')
     def getSingleResult(self, c):
         """
         Acquires the result of a single reading requested earlier
-        Output:
-        The raw data is a binary expression where each 0 corresponds to no photons
-        and 1 corresponds to a hit per one clock cycle of the FPGA clock.
-        The function compresses the data and put into a 2D numpy array by doing the following:
-        1. The binary data is split into a list of bytes (LB), and each byte gets converted to decimal
-        where most bytes are 0 as no counts took place in 8 clock cycles
-        2. We return a 2D numpy array, where the first row is the position of nonzero elements in LB
-        and the second row are those corresponding elements.
-        There operations have been found to be much faster than the data transfer rate from FPGA
+        The output are the time tags of arriving photons
         """
-        #may need to implement a timeout by checking if  self.singleReadingDeferred has fired by asking
-        #of self.inRequest and only yielding to self.singleReadingDeferred when False
         if self.singleReadingDeferred is None: raise "Single reading was not previously requested"
         raw = yield self.singleReadingDeferred
         self.singleReadingDeferred = None
-        data = numpy.fromstring(raw, dtype = numpy.uint16)        
-        nzindeces = numpy.array(data.nonzero()[0], dtype=numpy.int)
-        nzelems = numpy.array(data[nzindeces],dtype=numpy.int)
-        result = numpy.vstack((nzindeces,nzelems)).transpose()
-        returnValue(((data.size, self.timelength, timeResolution),result))
+        data = numpy.fromstring(raw, dtype = numpy.uint16)
+        timetags = data.nonzero()[0]*timeResolution * 16
+        del(data)
+        returnValue(timetags)
         
     @setting(2, 'Set Time Length', timelength = 'v[s]', returns = '')
     def setTimeLength(self, c, timelength):
--- paulbox/PaulBoxServer.py
+++ paulbox/PaulBoxServer.py
@@ -1,6 +1,5 @@
 #Created on Jan 24, 2011
 #@author: christopherreilly, Michael Ramm
-
 """
 ### BEGIN NODE INFO
 [info]
@@ -18,138 +17,108 @@ message = 987654321
 timeout = 20
 ### END NODE INFO
 """
-
 import socket
-import shelve
 import os
 from labrad.server import LabradServer, setting
 
+PORT = 8880
+directory = '/home/lattice/Desktop/sequencer2/PulseSequences/protected/'
+
 class script:
     scriptname = ''
-    filemodtime = ''
     varlist = [];
 
-class PBError( Exception ):
-    pass
-
 class PaulBoxServer( LabradServer ):
-    #change nodename in environment vars
     name = 'Paul Box'
 
     def initServer( self ):
         self.connectSocket()
-        self.loadDatabase()
         self.loadScripts()
 
     def connectSocket( self ):
-        port = 8880
         self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
         local_ip = socket.gethostbyname( socket.gethostname() )
-        self.sock.connect( ( local_ip, port ) )
-
-    def loadDatabase( self ):
-        """
-        Load database.
-        """
-        self.db = {}
-
+        self.sock.connect( ( local_ip, PORT ) )
 
     def loadScripts( self ):
         """
-        Load scrips
-        
-        TODO: details
+        Loads available scripts from the local folder
         """
-
-        def parse_sequence( sequence_string ):
-            varlist = []
-            for line in sequence_string.splitlines():
-                setvarindex = line.find( "self.set_variable(" )
-                if setvarindex > -1: #if line contains 'self.set_variable'
-                    poundindex = line.find( "#" )
-                    if poundindex == -1 or poundindex > setvarindex: #if the line is not commented out ('#' after 'self.setvariable')
-                        end = line.find( ")" )
-                        relevantline = line[setvarindex + len( "self.set_variable(" ):end]
-                        values = relevantline.split( ',' )
-                        var_type = str( values[0].replace( '"', '' ) ) #gets rid of quotes by replacing them with nothing
-                        var_name = str( values[1].replace( '"', '' ) )
-                        default_val = str( values[2] )
-                        if len( values ) == 5:
-                            min_val = str( values[3] )
-                            max_val = str( values[4] )
-                        else:
-                            min_val = ''
-                            max_val = ''
-                        varlist.append( [var_name, var_type, default_val, min_val, max_val] )
-            return varlist
-
-        directory = '/home/lattice/Desktop/sequencer2/PulseSequences/protected/'
-
-        for filename in os.listdir( directory ):
+        self.db = {}
+        files = [f for f in os.listdir(directory) if f.lower().endswith('.py')]
+        for filename in files:
             path = directory + filename
             try:
                 fobj = open( path )
                 sequence_string = fobj.read()
                 fobj.close()
-                modtime = os.stat( path ).st_mtime
             except:
-                raise RuntimeError( "Error while loading sequence:" + str( filename ) )
+                raise Exception( "Error while loading sequence:" + str( filename ) )
             newscript = script()
             newscript.scriptname = filename
-            newscript.filemodtime = modtime
-            newscript.varlist = parse_sequence( sequence_string )
-            self.db[filename] = newscript;
-
-    def sendPB( self, toSend ):
-        """
-        Lower level sending
-        """
+            newscript.varlist = self._parse_sequence( sequence_string )
+            self.db[filename] = newscript
+    
+    def _parse_sequence(self , sequence_string):
+        """Goes through the sequence files and figures out which variables should be set default values and range"""
+        varlist = []
+        for line in sequence_string.splitlines():
+            setvarindex = line.find( "self.set_variable(" )
+            if setvarindex > -1: #if line contains 'self.set_variable'
+                poundindex = line.find( "#" )
+                if poundindex == -1 or poundindex > setvarindex: #if the line is not commented out ('#' after 'self.setvariable')
+                    end = line.find( ")" )
+                    relevantline = line[setvarindex + len( "self.set_variable(" ):end]
+                    values = relevantline.split( ',' )
+                    var_type = str( values[0].replace( '"', '' ) ) #gets rid of quotes by replacing them with nothing
+                    var_name = str( values[1].replace( '"', '' ) )
+                    default_val = str( values[2] )
+                    if len( values ) == 5:
+                        min_val = str( values[3] )
+                        max_val = str( values[4] )
+                    else:
+                        min_val = ''
+                        max_val = ''
+                    varlist.append( [var_name, var_type, default_val, min_val, max_val] )
+        return varlist
+    
+    def _sendPB( self, toSend ):
         self.sock.sendall( toSend )
 
-    def recPB( self ):
-        """
-        Lower level receiving
-        """
+    def _recPB( self ):
         return '\n'.join( self.sock.recv( 4 * 8192 ) for i in range( 2 ) )
 
-    @setting( 0, 'Send command',
-              scriptName = 's: script name',
-              inputList = '*2s: input values',
-              returns = 's: response from PB' )
+    @setting( 0, 'Send command', scriptName = 's: script name', inputList = '*2s: input values',returns = 's: response from PB' )
     def sendCommand( self, c, scriptName, inputList ):
         """
         Send command to Paul's Box.
         """
         if len( filter( lambda x: len( x ) == 3, inputList ) ) != len( inputList ):
-            raise PBError( 'Input parameters must be in form of a list of 3 entry lists' )
+            raise Exception( 'Input parameters must be in form of a list of 3 entry lists' )
 
         def flattenInputList( li ):
             return ';'.join( ','.join( i ) for i in li )
 
         toSend = 'NAME,%s;' % scriptName + flattenInputList( inputList )
-        print toSend
-        self.sendPB( toSend )
-        resp = self.recPB()
+        self._sendPB( toSend )
+        resp = self._recPB()
         return resp
     
-    @setting(1,'Get available scripts',
-             returns = '*s: list of available scripts')
+    @setting(1,'Get available scripts',returns = '*s: list of available scripts')
     def availableScripts(self, c ):
         return self.db.keys()
     
-    @setting(2,'Get variable list',
-             script = 's: name of script',
-             returns = '*2s: list of script\'s variables')
+    @setting(2,'Get variable list',script = 's: name of script',returns = '*2s: list of script\'s variables')
     def getVariables(self, c, script):
         return self.db[script].varlist
+    
+    @setting(3, 'Reload Scripts', returns = '')
+    def reloadScripts(self, c):
+        self.loadScripts()
+    
+    def stopServer(self):
+        self.sock.close()
         
 if __name__ == "__main__":
     from labrad import util
     util.runServer(PaulBoxServer())
-
-
-
-
-
-
-
