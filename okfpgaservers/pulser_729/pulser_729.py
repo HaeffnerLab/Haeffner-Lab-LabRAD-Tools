f7b897e3a9c2934003e7cf531aba3afd6c04fd74
Haeffner Lab <haeffnerlab@gmail.com>
2012-08-07 11:18:52 -0700

    new pulser

--- okfpgaservers/pulser_729/pulser_729.py
+++ okfpgaservers/pulser_729/pulser_729.py
@@ -25,6 +25,12 @@ from labrad.types import Error
 class Pulser_729(LabradServer):
     
     name = 'pulser_729'
+    users = {
+             'lattice':(),
+             'sqip':(),
+             'cct':(),
+             'tester':()
+             }
     
     @inlineCallbacks    
     def initServer(self):
@@ -70,19 +76,35 @@ class Pulser_729(LabradServer):
         yield self.inCommunication.acquire()
         yield deferToThread(self.api.initializeDDS)
         self.inCommunication.release()
-    
-    @setting(3, "Control", shouldSet = 'b', returns = '(ww)')
-    def getControl(self, c, shouldSet = False):
+     
+    @setting(3, "Control", shouldSet = 'b', returns = 's')
+    def control(self, c, shouldSet = False):
         if shouldSet:
-            self.in_control = c.ID
-        if self.in_control is None: return (0,0)
+            user = c.get('user')
+            if user is None: raise Error(code = 2, msg = 'User Not Set')
+            self.in_control = user
+        if self.in_control is None: return ''
         return self.in_control
+    
+    @setting(4, 'User', name = 's', returns = 's')
+    def user(self, c, name = None):
+        if name is not None:
+            if name not in self.users.keys(): raise Exception ("Select users among {}".format(self.users.keys()))
+            c['user'] = name
+        setName = c.get('user')
+        if setName is None: setName = ''
+        return setName
     
     def check_control(self, c):
         '''checks for which context controls the channel'''
-        if self.in_control is None:
-            self.in_control = c
-        elif self.in_control != c:
+        user = c.get('user')
+        #if user name not set, raise error
+        if user is None: raise Error(code = 2, msg = 'User Not Set')
+        #if no one is in control, firt one to access gets control
+        elif self.in_control is None:
+            self.in_control = user
+        #if someone else already in control, notify
+        elif self.in_control != user:
             raise Error(code = 1, msg = 'Not in Control')
     
     def _programDDSSequence(self, program):
@@ -99,7 +121,9 @@ class Pulser_729(LabradServer):
         return d
     
     def expireContext(self, c):
-        if c.ID == self.in_control:
+        user = c.get('user')
+        #user gives up control on exit
+        if (user == self.in_control):
             self.in_control = None
         
 if __name__ == "__main__":
