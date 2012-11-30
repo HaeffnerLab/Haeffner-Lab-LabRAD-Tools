15a8301476ec22e0e5fa11bd5f63c24bf664b42e
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-11-29 18:30:25 -0800

    changing to linear fit

--- abstractdevices/SD_tracker/SD_tracker_config.py
+++ abstractdevices/SD_tracker/SD_tracker_config.py
@@ -3,7 +3,7 @@ class config(object):
     double_pass_passes = 2
     double_pass_direction = -1 #1 means add frequencies, -1 subtracts
     
-    fit_order = 2 #order of polynomial for fitting
+    fit_order = 1 #order of polynomial for fitting
     
     keep_measurements = 24 * 3600 #seconds, how long to keep measurements
     
