31433aad70e59f46da1294cdd69b91876f3d257b
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-01-16 11:31:13 -0800

    removing 397 channel

--- serialdevices/multiplexer/Multiplexer_config.py
+++ serialdevices/multiplexer/Multiplexer_config.py
@@ -4,10 +4,9 @@ class multiplexer_config(object):
     contains dictionary info in the form of
     {channel_name: (channel_number, wavelength),.. }
     '''
-    info = {'397': (5, '397'),
-            '422': (4, '422'),
+    info = {'422': (4, '422'),
             '866': (6, '866'),
-            '397 Single Pass': (11, '397'),
+            '397 Single Pass': (5, '397'),
             '729': (10, '729'),
             '397 diode': (2, '397'),
             '854': (12, '854'),   
