42b9ac1d12177e5c81380fdd412d48dc64980d5c
Dylan Gorman <dgorman@berkeley.edu>
2013-02-25 16:56:57 -0800

    prepaing for merge

--- abstractdevices/DacConfiguration.py
+++ abstractdevices/DacConfiguration.py
@@ -16,7 +16,6 @@ Stores complete information for each DAC channel
         else:
             self.name = name
 
-
 class hardwareConfiguration(object):
     numElectrodes = 23
     numSmaOuts = 5
@@ -26,30 +25,32 @@ class hardwareConfiguration(object):
     pulseTriggered = False # Set to True to have the DAC voltages advance by a pulse trigger, else update as each value is written
     maxCache = 126 # Max number of voltages which can be cached to the DAC
     
-    DACDict = {        
-        '1 ': channelConfiguration(16, trapElectrodeNumber = 1),
-        '2 ': channelConfiguration(17, trapElectrodeNumber = 2),
-        '3 ': channelConfiguration(18, trapElectrodeNumber = 3),
-        '4 ': channelConfiguration(3 , trapElectrodeNumber = 4),
-        '5 ': channelConfiguration(28, trapElectrodeNumber = 5),
-        '6 ': channelConfiguration(14, trapElectrodeNumber = 6),
-        '7 ': channelConfiguration(27, trapElectrodeNumber = 7),
+    elecDict = {        
+        '1 ': channelConfiguration(28, trapElectrodeNumber = 1),
+        '2 ': channelConfiguration(27, trapElectrodeNumber = 2),
+        '3 ': channelConfiguration(24, trapElectrodeNumber = 3),
+        '4 ': channelConfiguration(5, trapElectrodeNumber = 4),
+        '5 ': channelConfiguration(14, trapElectrodeNumber = 5),
+        '6 ': channelConfiguration(18, trapElectrodeNumber = 6),
+        '7 ': channelConfiguration(16, trapElectrodeNumber = 7),
         '8 ': channelConfiguration(13, trapElectrodeNumber = 8),
-        '9 ': channelConfiguration(10, trapElectrodeNumber = 9),
-        '10': channelConfiguration(5 , trapElectrodeNumber = 10),
-        '11': channelConfiguration(12, trapElectrodeNumber = 11),
-        '12': channelConfiguration(24, trapElectrodeNumber = 12),
-        '13': channelConfiguration(11, trapElectrodeNumber = 13),
-        '14': channelConfiguration(23, trapElectrodeNumber = 14),
-        '15': channelConfiguration(26, trapElectrodeNumber = 15),
-        '16': channelConfiguration(1 , trapElectrodeNumber = 16),        
-        '17': channelConfiguration(4 , trapElectrodeNumber = 17),        
-        '18': channelConfiguration(21, trapElectrodeNumber = 18),
-        '19': channelConfiguration(8 , trapElectrodeNumber = 19),
-        '20': channelConfiguration(20, trapElectrodeNumber = 20),
-        '21': channelConfiguration(19, trapElectrodeNumber = 21),
-        '22': channelConfiguration(7 , trapElectrodeNumber = 22),
-        '23': channelConfiguration(6 , trapElectrodeNumber = 23),
-        #-----------------------SMA Outs------------------------#
-        'RF bias': channelConfiguration(9, smaOutNumber = 1, name = 'RF bias', allowedVoltageRange = (-2.0, 0)),
+        '9 ': channelConfiguration(11, trapElectrodeNumber = 9),
+        '10': channelConfiguration(1, trapElectrodeNumber = 10),
+        '11': channelConfiguration(6, trapElectrodeNumber = 11),
+        '12': channelConfiguration(7, trapElectrodeNumber = 12),
+        '13': channelConfiguration(8, trapElectrodeNumber = 13),
+        '14': channelConfiguration(26, trapElectrodeNumber = 14),
+        '15': channelConfiguration(25, trapElectrodeNumber = 15),
+        '16': channelConfiguration(23, trapElectrodeNumber = 16),        
+        '17': channelConfiguration(4, trapElectrodeNumber = 17),        
+        '18': channelConfiguration(19, trapElectrodeNumber = 18),
+        '19': channelConfiguration(17, trapElectrodeNumber = 19),
+        '20': channelConfiguration(3, trapElectrodeNumber = 20),
+        '21': channelConfiguration(20, trapElectrodeNumber = 21),
+        '22': channelConfiguration(12, trapElectrodeNumber = 22),
+        '23': channelConfiguration(10, trapElectrodeNumber = 23),
+        }
+
+    smaDict = {
+        'RF bias': channelConfiguration(9, smaOutNumber = 1, name = 'RF bias', boardVoltageRange = (-10., 10.), allowedVoltageRange = (-2.0, 0)),
         }
