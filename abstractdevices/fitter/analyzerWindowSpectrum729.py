8d2994e8e7afef855e17397e00f65b7653762292
Hlab, lab-197 <haeffnerlab@gmail.com>
2014-03-21 14:41:02 -0700

    move fitter to abstract devices and add rabi power flop fitter

--- abstractdevices/fitter/analyzerWindowSpectrum729.py
+++ abstractdevices/fitter/analyzerWindowSpectrum729.py
@@ -11,3 +11,12 @@ class analyzerWindow729(AnalyzerWindow):
         custom_steps = [('Center', 2e-3), ('Background', 1e-2)]
         self.axes.set_xlabel('MHz')
         self.update_steps(custom_steps)
+        
+class analyzerWindowPowerFlop(AnalyzerWindow):
+       
+    def __init__(self, fitting_parameters, auto_accept, interface):
+        super(analyzerWindowPowerFlop, self).__init__(fitting_parameters, auto_accept, interface)
+        
+    def perform_customization(self):
+        self.axes.set_ylim([0,1])
+        self.axes.set_xlabel('dBm')
--- abstractdevices/fitter/fitting_interface.py
+++ abstractdevices/fitter/fitting_interface.py
@@ -1,9 +1,13 @@
 from lorentzian import Lorentzian
+from rabi_power_flop import Rabi_power_flop
 
 from analyzerWindow import AnalyzerWindow
-from analyzerWindowSpectrum729 import analyzerWindow729
+from analyzerWindowSpectrum729 import analyzerWindow729, analyzerWindowPowerFlop
 
-fitting_models = [Lorentzian]
+fitting_models = {
+                  Lorentzian:analyzerWindow729,
+                  Rabi_power_flop:analyzerWindowPowerFlop,
+                  }
 
 class FittingInterface(object):
     def __init__(self):
@@ -41,7 +45,9 @@ class FittingInterface(object):
         self.active_model.setUserParameters(self.manual_parameters)
         fitX, fitY = self.active_model.fit()
         fitting_parameters = self.active_model.get_parameter_info()
-        self.gui = analyzerWindow729(fitting_parameters, self.auto_accept, self)
+        model_cls = self.active_model.__class__
+        window_cls = fitting_models[model_cls]
+        self.gui = window_cls(fitting_parameters, self.auto_accept, self)
         self.gui.plot(dataX, dataY, '*k')
         self.gui.plotfit(fitX, fitY)
         if self.auto_accept:
