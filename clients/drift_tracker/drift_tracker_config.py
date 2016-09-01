6ac40f5057d6cb9ca5b221380e33143f21a3e089
bhemmerling <boerge.hemmerling@gmail.com>
2016-09-01 13:14:52 -0700

    Trying to remove drift_tracker_config.py

--- clients/drift_tracker/drift_tracker_config.py
+++ clients/drift_tracker/drift_tracker_config.py
@@ -6,7 +6,7 @@ class config_729_tracker(object):
     frequency_limit = (-60, 140) #MHz
     saved_lines_729 = ['729Experiments','saved_lines_729']
     favorites = {'S+1/2D-3/2':'OP', 'S-1/2D+3/2':'Right OP', 'S-1/2D-5/2':'carrier -1/2-5/2', 'S-1/2D-1/2':'carrier -1/2-1/2'}
-    initial_selection = ['S-1/2D-1/2', 'S-1/2D-5/2'] # select these lines in the entry table at startup, has to be the name that is saved, i.e. instead of OP, S+1/2D-3/2
-    initial_values = [-19.8, -30.2374] # initial frequencies of the lines at startup (in MHz)
+    initial_selection = ['S-1/2D-5/2', 'S+1/2D-3/2'] # select these lines in the entry table at startup, has to be the name that is saved, i.e. instead of OP, S+1/2D-3/2
+    initial_values = [-30.8372, -34.55] # initial frequencies of the lines at startup (in MHz)
 
 
