94177430aa5413a0a45d4d293d1259b54183c746
Dylan J MacDonald Gorman <dylan@nfs.haeffnerlab.edu>
2017-04-28 13:38:16 -0700

    building sequences no longer needs required_parameters

--- devel/bum/sequences/subsequences/ExampleSubsequence.py
+++ devel/bum/sequences/subsequences/ExampleSubsequence.py
@@ -1,20 +1,11 @@
-from common.okfpgaservers.pulser.pulse_sequences.pulse_sequence import pulse_sequence 
+from common.devel.bum.sequences.pulse_sequence import pulse_sequence
+from labrad.units import WithUnit as U
 
-class doppler_cooling(pulse_sequence):
-    
+class example(pulse_sequence):
     
     required_parameters = [
-                           ('DopplerCooling','doppler_cooling_frequency_397'), 
-                           ('DopplerCooling','doppler_cooling_amplitude_397'), 
-                           ('DopplerCooling','doppler_cooling_frequency_866'), 
-                           ('DopplerCooling','doppler_cooling_amplitude_866'), 
-                           ('DopplerCooling','doppler_cooling_duration'),
-                           ('DopplerCooling','doppler_cooling_repump_additional')
                            ]
     
     def sequence(self):
-        p = self.parameters.DopplerCooling
-        repump_duration = p.doppler_cooling_duration + p.doppler_cooling_repump_additional
-        self.addDDS ('397',self.start, p.doppler_cooling_duration, p.doppler_cooling_frequency_397, p.doppler_cooling_amplitude_397)
-        self.addDDS ('866',self.start, repump_duration, p.doppler_cooling_frequency_866, p.doppler_cooling_amplitude_866)
-        self.end = self.start + repump_duration
+        print self.parameters['DopplerCooling.duration']
+        self.end = self.start + U(5, 'us')
