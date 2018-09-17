0db7cc535eddd08a4ecec5e54b84394c4a11bfe4
berkeleyions <haeffner-lab@berkeley.edu>
2018-09-17 15:52:49 -0700

    use client_config to store global server address

--- client_config.py
+++ client_config.py
@@ -1,6 +1,6 @@
 class client_info(object):
 	
 	client_list = ['lattice', 'cct', 'sqip', 'space time']
-	client_name = 'lattice'
-	global_address = '192.169.168.86'
+	client_name = 'cct'
+	global_address = '192.168.169.86'
 	global_password = ''
