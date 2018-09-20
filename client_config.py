d934c7118171ee671d60b1ad6e002cc99674ec2b
berkeleyions <haeffner-lab@berkeley.edu>
2018-09-20 10:07:47 -0700

    add host IP to client_config

--- client_config.py
+++ client_config.py
@@ -1,6 +1,6 @@
 class client_info(object):
 	
 	client_list = ['lattice', 'cct', 'sqip', 'space time']
-	client_name = 'cct'
+	client_name = 'lattice'
 	global_address = '192.168.169.86'
 	global_password = ''
