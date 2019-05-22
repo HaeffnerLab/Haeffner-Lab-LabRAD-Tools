d57732d94d54cb7a8f69200e815330ab66501125
staq <haeffnerlab@gmail.com>
2019-05-21 17:42:40 -0700

    staq

--- clients/CONFIG_EDITOR_config_example.py
+++ clients/CONFIG_EDITOR_config_example.py
@@ -1,5 +1,5 @@
 # config file for CONFIG_EDITOR
 
 # folder where to look for config files
-labrad_folders = ['/home/space-time/LabRAD/common', '/home/space-time/LabRAD/space-time']
+labrad_folders = ['/home/staq/Labrad/common', '/home/staq/Labrad/staq']
 
